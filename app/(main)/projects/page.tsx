08aed3fae5262c62be953ea4b612e131e7a0ba68
Ashwin-Khowala <ashwinkhowala1@gmail.com>
2025-06-05 01:19:23 +0530

    made the azure blob storage working

--- app/(main)/projects/page.tsx
+++ app/(main)/projects/page.tsx
@@ -1,6 +1,15 @@
+"use client";
 import ProjectCard from "@/components/projectComponent/ProjectCard";
 
 export default function page() {
+  const response = fetch('http://localhost:3000/api/projects', {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+  const projects = response.then(res => res.json());
+  console.log(projects);
   return (
     <div>
         <h1 className="text-2xl font-bold mb-4">Projects</h1>
